54cc539cb797226df26943f66a4b1a2140dc3afa
Sean Cramsey <cramseys@oregonstate.edu>
2017-12-04 18:42:39 -0800

    Add image label thing, (david sucks juicy cacks)

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => Html::img('MartiniThing.png', ['alt'=>Yii::$app->name]),
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
