16a6daf7d3ab1d1a320a4ad30b7b47cc6dd3fd8e
David Okubo <okubod@oregonstate.edu>
2017-12-04 18:17:17 -0800

    Update view pages fro Drink and Ingredients

--- views/ingredients/view.php
+++ views/ingredients/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Ingredients */
 
-$this->title = $model->ingredientid;
+$this->title = $model->ingredientname;
 $this->params['breadcrumbs'][] = ['label' => 'Ingredients', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
