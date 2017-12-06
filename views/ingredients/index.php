9604df75605a3939509c4ffe4476fdecb12f2661
David Okubo <okubod@oregonstate.edu>
2017-12-05 18:49:49 -0800

    fixed

--- views/ingredients/index.php
+++ views/ingredients/index.php
@@ -31,7 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ['label' => 'Type',
             'value' => 'type'],
             ['label' => 'Family',
-            'value''family'],
+            'value' => 'family'],
 
         ],
     ]); ?>
