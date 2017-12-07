8a1508fd81da24a58abdb69386673f9eba3ba978
David Okubo <okubod@oregonstate.edu>
2017-12-06 18:36:21 -0800

    forgot the form

--- views/ingredientslist/_form.php
+++ views/ingredientslist/_form.php
@@ -17,7 +17,7 @@ use yii\db\Query;
 
     <?php
       $query = new query();
-      $query->select('ingredientid')->where("ingredientid NOT IN (SELECT ingredientid FROM ingredientlist WHERE drinkid = $model->drinkid)");
+      $query->select('ingredientid')->from('ingredientlist')->where("ingredientid NOT IN (SELECT ingredientid FROM ingredientlist WHERE drinkid = $model->drinkid)");
       //$list = Ingredients::findAll(array("ingredientid NOT IN (SELECT ingredientid FROM ingredientlist WHERE drinkid = $model->drinkid)",));
       //$list = $model->findall('ingredientid');
       $items = ArrayHelper::map($query, 'ingredientid', 'ingredientname');
